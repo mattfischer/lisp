66025874721fce26dbe78226341893358da4b0ed
Matt Fischer <mattfischer84@gmail.com>
2017-05-17 22:19:48 -0500

    Added better type checking

--- Eval.cpp
+++ Eval.cpp
@@ -4,28 +4,106 @@
 #include <iostream>
 #include <cstring>
 
+std::ostream &operator<<(std::ostream &o, Object::Type type)
+{
+	switch (type) {
+	case Object::TypeNone:
+		o << "nil";
+		break;
+
+	case Object::TypeInt:
+		o << "int";
+		break;
+
+	case Object::TypeString:
+		o << "string";
+		break;
+
+	case Object::TypeAtom:
+		o << "atom";
+		break;
+
+	case Object::TypeCons:
+		o << "cons";
+		break;
+	}
+
+	return o;
+}
+
+bool checkType(Object *object, Object::Type type)
+{
+	Object::Type objectType;
+
+	if (object) {
+		objectType = object->type();
+	} else {
+		objectType = Object::TypeNone;
+	}
+
+	if (objectType != type) {
+		std::cerr << "Error: Type mismatch on " << object << " (expected " << type << " got " << objectType << ")" << std::endl;
+		return false;
+	}
+
+	return true;
+}
+
+bool checkLength(Object *object, int length)
+{
+	if (!checkType(object, Object::TypeCons)) {
+		return false;
+	}
+
+	Object *cons = object;
+	int objectLength = 0;
+	while (cons) {
+		objectLength++;
+		cons = cons->cdrValue();
+	}
+
+	if (objectLength != length) {
+		std::cerr << "Error: List " << object << " is of incorrect length (expected " << length << " got " << objectLength << ")" << std::endl;
+		return false;
+	}
+
+	return true;
+}
+
+Object *listItem(Object *object, int index)
+{
+	Object *cons = object;
+	for (int i = 0; i < index; i++) {
+		if (!cons) {
+			return 0;
+		}
+		cons = cons->cdrValue();
+	}
+
+	return cons->carValue();
+}
+
 Object *evalFunction(Object *object)
 {
 	Object *car = object->carValue();
-	if (car->type() != Object::TypeAtom) {
-		std::cerr << "Error: Type mismatch evaluating " << object << std::endl;
+
+	if(!checkType(car, Object::TypeAtom)) {
 		return 0;
 	}
 
 	if (!std::strcmp(car->stringValue(), "+")) {
-		int value = 0;
-		Object *cons = object->cdrValue();
-		while (cons) {
-			Object *consEval = eval(cons->carValue());
-			if (consEval->type() != Object::TypeInt) {
-				std::cerr << "Error: " << consEval << " is non-integral" << std::endl;
-				return 0;
-			}
-			value += consEval->intValue();
-			cons = cons->cdrValue();
+		if (!checkLength(object, 3)) {
+			return 0;
 		}
+
+		Object *a = listItem(object, 1);
+		Object *b = listItem(object, 2);
+		if (!checkType(a, Object::TypeInt) || !checkType(b, Object::TypeInt)) {
+			return 0;
+		}
+
 		Object *ret = new Object();
-		ret->setInt(value);
+		ret->setInt(a->intValue() + b->intValue());
 		return ret;
 	}
 
